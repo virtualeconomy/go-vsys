cd3068e9ccf3acd94761e75fbf117e2c1a054def
poiug07vsys <106374617+poiug07vsys@users.noreply.github.com>
2022-07-25 15:21:52 +0800

    tests: move tests body into internal implementation & add TestAsWhole

--- vsys/atomic_swap_ctrt_test.go
+++ vsys/atomic_swap_ctrt_test.go
@@ -17,7 +17,7 @@ func newTokCtrtWithTok(t *testing.T, by *Account) (*TokCtrtWithoutSplit, error) 
 
 	resp, err := tc.Issue(by, 1000, "")
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("newMakerTokCtrtWithTok: %w", err)
 	}
 	waitForBlock()
 	assertTxSuccess(t, string(resp.Id))
@@ -27,7 +27,7 @@ func newTokCtrtWithTok(t *testing.T, by *Account) (*TokCtrtWithoutSplit, error) 
 func newAtomicSwap(t *testing.T, by *Account) (*AtomicSwapCtrt, error) {
 	token, err := newTokCtrtWithTok(t, by)
 	if err != nil {
-		t.Fatal(err)
+		return nil, fmt.Errorf(": %w", err)
 	}
 	tokId, _ := token.TokId()
 	ac, err := RegisterAtomicSwapCtrt(by, string(tokId.B58Str()), "")
@@ -46,16 +46,12 @@ func newAtomicSwap(t *testing.T, by *Account) (*AtomicSwapCtrt, error) {
 	return ac, nil
 }
 
-func Test_AtomicSwapCtrt_Register(t *testing.T) {
-	tc, err := newTokCtrtWithTok(t, testAcnt0)
-	if err != nil {
-		t.Fatalf("Cannot get new maker token ctrt: %s\n", err.Error())
-	}
+func test_AtomicSwapCtrt_Register(t *testing.T, acnt *Account, tc *TokCtrtWithoutSplit) *AtomicSwapCtrt {
 	tokId, err := tc.TokId()
 	if err != nil {
 		t.Fatal(err)
 	}
-	ac, err := RegisterAtomicSwapCtrt(testAcnt0, string(tokId.B58Str()), "")
+	ac, err := RegisterAtomicSwapCtrt(acnt, string(tokId.B58Str()), "")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -65,12 +61,74 @@ func Test_AtomicSwapCtrt_Register(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	assert.Equal(t, testAcnt0.Addr, maker)
+	assert.Equal(t, acnt.Addr, maker)
 	tokIdFromCtrt, err := ac.TokId()
 	if err != nil {
 		t.Error(err)
 	}
 	assert.Equal(t, tokId, tokIdFromCtrt)
+	return ac
+}
+
+func Test_AtomicSwapCtrt_Register(t *testing.T) {
+	tc, err := newTokCtrtWithTok(t, testAcnt0)
+	if err != nil {
+		t.Fatalf("Cannot get new maker token ctrt: %s\n", err.Error())
+	}
+	test_AtomicSwapCtrt_Register(t, testAcnt0, tc)
+}
+
+func test_AtomicSwapCtrt_Lock(t *testing.T, maker, taker *Account, makerCtrt, takerCtrt *AtomicSwapCtrt) {
+	makerBalInit, _ := makerCtrt.GetCtrtBal(string(maker.Addr.B58Str()))
+	takerBalInit, _ := takerCtrt.GetCtrtBal(string(taker.Addr.B58Str()))
+
+	makerLockAmount := 10.0
+	makerLockTimestamp := time.Now().Unix() + 1800
+	makerPuzzlePlain := "abc"
+	puzzleBytes := Sha256Hash([]byte(makerPuzzlePlain))
+
+	makerLockTxInfo, err := makerCtrt.Lock(maker, makerLockAmount, string(taker.Addr.B58Str()), puzzleBytes, makerLockTimestamp, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	waitForBlock()
+	makerLockTxId := string(makerLockTxInfo.Id)
+	assertTxSuccess(t, makerLockTxId)
+
+	makerSwapOwner, _ := makerCtrt.GetSwapOwner(makerLockTxId)
+	assert.Equal(t, maker.Addr, makerSwapOwner)
+	makerSwapRecipient, _ := makerCtrt.GetSwapRecipient(makerLockTxId)
+	assert.Equal(t, taker.Addr, makerSwapRecipient)
+	makerSwapAmount, _ := makerCtrt.GetSwapAmount(makerLockTxId)
+	assert.Equal(t, makerLockAmount, makerSwapAmount.Amount())
+	makerSwapTimestamp, _ := makerCtrt.GetSwapExpiredTime(makerLockTxId)
+	assert.Equal(t, makerLockTimestamp, makerSwapTimestamp.UnixTs())
+	makerSwapStatus, _ := makerCtrt.GetSwapStatus(makerLockTxId)
+	assert.Equal(t, true, makerSwapStatus)
+	makerPuzzle, _ := makerCtrt.GetSwapPuzzle(makerLockTxId)
+	decoded, err := B58Decode(string(makerPuzzle))
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, Sha256Hash([]byte(makerPuzzlePlain)), decoded)
+	makerBalAfterLock, _ := makerCtrt.GetCtrtBal(string(maker.Addr.B58Str()))
+	assert.Equal(t, makerBalInit.Amount()-makerLockAmount, makerBalAfterLock.Amount())
+
+	// Taker lock here
+	takerLockAmount := 5.0
+	takerLockTimestamp := time.Now().Unix() + 1500
+	takerLockTxInfo, err := takerCtrt.Lock(
+		taker,
+		takerLockAmount,
+		string(maker.Addr.B58Str()),
+		decoded, // Got decoded puzzle bytes from previous checks
+		takerLockTimestamp,
+		"",
+	)
+	waitForBlock()
+	assertTxSuccess(t, string(takerLockTxInfo.Id))
+	takerBalAfterLock, _ := takerCtrt.GetCtrtBal(string(taker.Addr.B58Str()))
+	assert.Equal(t, takerBalInit.Amount()-takerLockAmount, takerBalAfterLock.Amount())
 }
 
 func Test_AtomicSwapCtrt_Lock(t *testing.T) {
@@ -89,16 +147,17 @@ func Test_AtomicSwapCtrt_Lock(t *testing.T) {
 	if err := g.Wait(); err != nil {
 		t.Fatal(err)
 	}
+	test_AtomicSwapCtrt_Lock(t, testAcnt0, testAcnt1, makerCtrt, takerCtrt)
+}
 
-	makerBalInit, _ := makerCtrt.GetCtrtBal(string(testAcnt0.Addr.B58Str()))
-	takerBalInit, _ := takerCtrt.GetCtrtBal(string(testAcnt1.Addr.B58Str()))
-
+func test_AtomicSwapCtrt_Solve(t *testing.T, maker, taker *Account, makerCtrt, takerCtrt *AtomicSwapCtrt) {
+	// Maker lock
 	makerLockAmount := 10.0
 	makerLockTimestamp := time.Now().Unix() + 1800
 	makerPuzzlePlain := "abc"
 	puzzleBytes := Sha256Hash([]byte(makerPuzzlePlain))
 
-	makerLockTxInfo, err := makerCtrt.Lock(testAcnt0, makerLockAmount, string(testAcnt1.Addr.B58Str()), puzzleBytes, makerLockTimestamp, "")
+	makerLockTxInfo, err := makerCtrt.Lock(maker, makerLockAmount, string(taker.Addr.B58Str()), puzzleBytes, makerLockTimestamp, "")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -106,40 +165,57 @@ func Test_AtomicSwapCtrt_Lock(t *testing.T) {
 	makerLockTxId := string(makerLockTxInfo.Id)
 	assertTxSuccess(t, makerLockTxId)
 
-	makerSwapOwner, _ := makerCtrt.GetSwapOwner(makerLockTxId)
-	assert.Equal(t, testAcnt0.Addr, makerSwapOwner)
-	makerSwapRecipient, _ := makerCtrt.GetSwapRecipient(makerLockTxId)
-	assert.Equal(t, testAcnt1.Addr, makerSwapRecipient)
-	makerSwapAmount, _ := makerCtrt.GetSwapAmount(makerLockTxId)
-	assert.Equal(t, makerLockAmount, makerSwapAmount.Amount())
-	makerSwapTimestamp, _ := makerCtrt.GetSwapExpiredTime(makerLockTxId)
-	assert.Equal(t, makerLockTimestamp, makerSwapTimestamp.UnixTs())
-	makerSwapStatus, _ := makerCtrt.GetSwapStatus(makerLockTxId)
-	assert.Equal(t, true, makerSwapStatus)
 	makerPuzzle, _ := makerCtrt.GetSwapPuzzle(makerLockTxId)
-	decoded, err := B58Decode(string(makerPuzzle))
+	puzzleBytes2, err := B58Decode(string(makerPuzzle))
 	if err != nil {
 		t.Fatal(err)
 	}
-	assert.Equal(t, Sha256Hash([]byte(makerPuzzlePlain)), decoded)
-	makerBalAfterLock, _ := makerCtrt.GetCtrtBal(string(testAcnt0.Addr.B58Str()))
-	assert.Equal(t, makerBalInit.Amount()-makerLockAmount, makerBalAfterLock.Amount())
 
 	// Taker lock here
 	takerLockAmount := 5.0
 	takerLockTimestamp := time.Now().Unix() + 1500
 	takerLockTxInfo, err := takerCtrt.Lock(
-		testAcnt1,
+		taker,
 		takerLockAmount,
-		string(testAcnt0.Addr.B58Str()),
-		decoded, // Got decoded puzzle bytes from previous checks
+		string(maker.Addr.B58Str()),
+		puzzleBytes2,
 		takerLockTimestamp,
 		"",
 	)
 	waitForBlock()
 	assertTxSuccess(t, string(takerLockTxInfo.Id))
-	takerBalAfterLock, _ := takerCtrt.GetCtrtBal(string(testAcnt1.Addr.B58Str()))
-	assert.Equal(t, takerBalInit.Amount()-takerLockAmount, takerBalAfterLock.Amount())
+
+	// maker solve
+	makerSolveTxInfo, err := takerCtrt.Solve(maker, string(takerLockTxInfo.Id), makerPuzzlePlain, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	waitForBlock()
+	assertTxSuccess(t, string(makerSolveTxInfo.Id))
+
+	dict_data, err := maker.Chain.NodeAPI.GetTxInfo(string(makerSolveTxInfo.Id))
+	if err != nil {
+		t.Fatal(err)
+	}
+	funcData := dict_data.(*ExecCtrtFuncTxInfoResp).FuncData
+	decoded, err := B58Decode(string(funcData))
+	if err != nil {
+		t.Fatal(err)
+	}
+	ds, err := NewDataStackFromBytes(decoded)
+	if err != nil {
+		t.Fatal(err)
+	}
+	revealedSecret := string(ds[1].DataBytes())
+	assert.Equal(t, makerPuzzlePlain, revealedSecret)
+
+	// taker solve
+	takerSolveTxInfo, err := makerCtrt.Solve(taker, makerLockTxId, revealedSecret, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	waitForBlock()
+	assertTxSuccess(t, string(takerSolveTxInfo.Id))
 }
 
 func Test_AtomicSwapCtrt_Solve(t *testing.T) {
@@ -158,14 +234,17 @@ func Test_AtomicSwapCtrt_Solve(t *testing.T) {
 	if err := g.Wait(); err != nil {
 		t.Fatal(err)
 	}
+	test_AtomicSwapCtrt_Solve(t, testAcnt0, testAcnt1, makerCtrt, takerCtrt)
+}
 
-	// Maker lock
+func test_AtomicSwapCtrt_ExpWithdraw(t *testing.T, acnt0, acnt1 *Account, makerCtrt *AtomicSwapCtrt) {
 	makerLockAmount := 10.0
-	makerLockTimestamp := time.Now().Unix() + 1800
+	makerLockTimestamp := time.Now().Unix() + 8
 	makerPuzzlePlain := "abc"
 	puzzleBytes := Sha256Hash([]byte(makerPuzzlePlain))
 
-	makerLockTxInfo, err := makerCtrt.Lock(testAcnt0, makerLockAmount, string(testAcnt1.Addr.B58Str()), puzzleBytes, makerLockTimestamp, "")
+	// Maker lock
+	makerLockTxInfo, err := makerCtrt.Lock(acnt0, makerLockAmount, string(acnt1.Addr.B58Str()), puzzleBytes, makerLockTimestamp, "")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -173,57 +252,19 @@ func Test_AtomicSwapCtrt_Solve(t *testing.T) {
 	makerLockTxId := string(makerLockTxInfo.Id)
 	assertTxSuccess(t, makerLockTxId)
 
-	makerPuzzle, _ := makerCtrt.GetSwapPuzzle(makerLockTxId)
-	puzzleBytes2, err := B58Decode(string(makerPuzzle))
-	if err != nil {
-		t.Fatal(err)
-	}
+	bal_old, err := makerCtrt.GetCtrtBal(string(acnt0.Addr.B58Str()))
 
-	// Taker lock here
-	takerLockAmount := 5.0
-	takerLockTimestamp := time.Now().Unix() + 1500
-	takerLockTxInfo, err := takerCtrt.Lock(
-		testAcnt1,
-		takerLockAmount,
-		string(testAcnt0.Addr.B58Str()),
-		puzzleBytes2,
-		takerLockTimestamp,
-		"",
-	)
-	waitForBlock()
-	assertTxSuccess(t, string(takerLockTxInfo.Id))
+	time.Sleep(6 * time.Second) // wait until lock expires
 
-	// maker solve
-	makerSolveTxInfo, err := takerCtrt.Solve(testAcnt0, string(takerLockTxInfo.Id), makerPuzzlePlain, "")
+	expWithdrawTxInfo, err := makerCtrt.ExpWithdraw(acnt0, makerLockTxId, "")
 	if err != nil {
 		t.Fatal(err)
 	}
 	waitForBlock()
-	assertTxSuccess(t, string(makerSolveTxInfo.Id))
+	assertTxSuccess(t, string(expWithdrawTxInfo.Id))
 
-	dict_data, err := testAcnt0.Chain.NodeAPI.GetTxInfo(string(makerSolveTxInfo.Id))
-	if err != nil {
-		t.Fatal(err)
-	}
-	funcData := dict_data.(*ExecCtrtFuncTxInfoResp).FuncData
-	decoded, err := B58Decode(string(funcData))
-	if err != nil {
-		t.Fatal(err)
-	}
-	ds, err := NewDataStackFromBytes(decoded)
-	if err != nil {
-		t.Fatal(err)
-	}
-	revealedSecret := string(ds[1].DataBytes())
-	assert.Equal(t, makerPuzzlePlain, revealedSecret)
-
-	// taker solve
-	takerSolveTxInfo, err := makerCtrt.Solve(testAcnt1, makerLockTxId, revealedSecret, "")
-	if err != nil {
-		t.Fatal(err)
-	}
-	waitForBlock()
-	assertTxSuccess(t, string(takerSolveTxInfo.Id))
+	bal, _ := makerCtrt.GetCtrtBal(string(acnt0.Addr.B58Str()))
+	assert.Equal(t, bal_old.Amount()+makerLockAmount, bal.Amount())
 }
 
 func Test_AtomicSwapCtrt_ExpWithdraw(t *testing.T) {
@@ -231,32 +272,26 @@ func Test_AtomicSwapCtrt_ExpWithdraw(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	test_AtomicSwapCtrt_ExpWithdraw(t, testAcnt0, testAcnt1, makerCtrt)
+}
 
-	makerLockAmount := 10.0
-	makerLockTimestamp := time.Now().Unix() + 8
-	makerPuzzlePlain := "abc"
-	puzzleBytes := Sha256Hash([]byte(makerPuzzlePlain))
-
-	// Maker lock
-	makerLockTxInfo, err := makerCtrt.Lock(testAcnt0, makerLockAmount, string(testAcnt1.Addr.B58Str()), puzzleBytes, makerLockTimestamp, "")
+func Test_AtomicSwapCtrt_AsWhole(t *testing.T) {
+	maker_tc, err := newTokCtrtWithTok(t, testAcnt0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	makerCtrt := test_AtomicSwapCtrt_Register(t, testAcnt0, maker_tc)
+	resp, err := maker_tc.Deposit(testAcnt0, string(makerCtrt.CtrtId.B58Str()), 1000, "")
 	if err != nil {
 		t.Fatal(err)
 	}
 	waitForBlock()
-	makerLockTxId := string(makerLockTxInfo.Id)
-	assertTxSuccess(t, makerLockTxId)
+	assertTxSuccess(t, string(resp.Id))
 
-	bal_old, err := makerCtrt.GetCtrtBal(string(testAcnt0.Addr.B58Str()))
-
-	time.Sleep(10 * time.Second) // wait for 10 seconds
-
-	expWithdrawTxInfo, err := makerCtrt.ExpWithdraw(testAcnt0, makerLockTxId, "")
+	takerCtrt, err := newAtomicSwap(t, testAcnt1)
 	if err != nil {
 		t.Fatal(err)
 	}
-	waitForBlock()
-	assertTxSuccess(t, string(expWithdrawTxInfo.Id))
-
-	bal, err := makerCtrt.GetCtrtBal(string(testAcnt0.Addr.B58Str()))
-	assert.Equal(t, bal_old.Amount()+makerLockAmount, bal.Amount())
+	test_AtomicSwapCtrt_Solve(t, testAcnt0, testAcnt1, makerCtrt, takerCtrt)
+	test_AtomicSwapCtrt_ExpWithdraw(t, testAcnt0, testAcnt1, makerCtrt)
 }
