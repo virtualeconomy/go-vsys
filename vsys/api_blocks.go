3fd550d794738253f5f4f272bfabb2c9fa351163
poiug07vsys <106374617+poiug07vsys@users.noreply.github.com>
2022-08-08 18:10:03 +0800

    feat: add missing Chain functions & docs

--- vsys/api_blocks.go
+++ vsys/api_blocks.go
@@ -1,6 +1,7 @@
 package vsys
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
@@ -20,4 +21,82 @@ func (na *NodeAPI) GetCurBlockHeight() (*CurBlockHeightResp, error) {
 		return nil, fmt.Errorf("GetCurBlockHeight: %s", resp.String())
 	}
 	return res, nil
+}
+
+type BlockResp struct {
+	Version       int           `json:"version"`
+	Timestamp     VSYSTimestamp `json:"timestamp"`
+	Reference     Str           `json:"reference"`
+	SPOSConsensus struct {
+		MintTime    VSYSTimestamp `json:"mintTime"`
+		MintBalance VSYS          `json:"mintBalance"`
+	} `json:"SPOSConsensus"`
+	ResourcePricingData struct {
+		Computation  int `json:"computation"`
+		Storage      int `json:"storage"`
+		Memory       int `json:"memory"`
+		RandomIO     int `json:"randomIO"`
+		SequentialIO int `json:"sequentialIO"`
+	} `json:"resourcePricingData"`
+	TransactionMerkleRoot Str `json:"TransactionMerkleRoot"`
+	Transactions          []struct {
+		Type               TxType        `json:"type"`
+		Id                 Str           `json:"id"`
+		Recipient          Str           `json:"recipient"`
+		Timestamp          VSYSTimestamp `json:"timestamp"`
+		Amount             Amount        `json:"amount"`
+		CurrentBlockHeight Height        `json:"currentBlockHeight"`
+		Status             Str           `json:"status"`
+		FeeCharged         VSYS          `json:"feeCharged"`
+	} `json:"transactions"`
+	Generator        Str    `json:"generator"`
+	Signature        Str    `json:"signature"`
+	Fee              VSYS   `json:"fee"`
+	Blocksize        int    `json:"blocksize"`
+	Height           Height `json:"height"`
+	TransactionCount int    `json:"transaction count"`
+}
+
+func (na *NodeAPI) GetLast() (*BlockResp, error) {
+	res := &BlockResp{}
+	resp, err := na.R().SetResult(res).Get("/blocks/last")
+	if err != nil {
+		return nil, fmt.Errorf("GetCurBlockHeight: %w", err)
+	}
+	if !resp.IsSuccess() {
+		return nil, fmt.Errorf("GetCurBlockHeight: %s", resp.String())
+	}
+	return res, nil
+}
+
+func (na *NodeAPI) GetBlockAt(h int) (*BlockResp, error) {
+	res := &BlockResp{}
+	resp, err := na.R().SetResult(res).Get(fmt.Sprintf("/blocks/at/%d", h))
+	if err != nil {
+		return nil, fmt.Errorf("GetBlockAt: %w", err)
+	}
+	if !resp.IsSuccess() {
+		return nil, fmt.Errorf("GetBlockAt: %s", resp.String())
+	}
+	return res, nil
+}
+
+func (na *NodeAPI) GetBlocksWithin(start, end int) ([]*BlockResp, error) {
+	res := make([]*BlockResp, 0)
+	resp, err := na.R().Get(fmt.Sprintf("/blocks/seq/%d/%d", start, end))
+	if err != nil {
+		return nil, fmt.Errorf("GetBlocksWithin: %w", err)
+	}
+	if !resp.IsSuccess() {
+		return nil, fmt.Errorf("GetBlocksWithin: %s", resp.String())
+	}
+	err = json.Unmarshal(resp.Bytes(), &res)
+	if err != nil {
+		return nil, fmt.Errorf("GetTxInfo: %w", err)
+	}
+	return res, nil
+}
+
+func (b *BlockResp) String() string {
+	return fmt.Sprintf("%T(%+v)", b, *b)
 }
--- vsys/chain.go
+++ vsys/chain.go
@@ -24,3 +24,27 @@ func (c *Chain) Height() (Height, error) {
 	}
 	return res.Height, nil
 }
+
+func (c *Chain) LastBlock() (*BlockResp, error) {
+	res, err := c.NodeAPI.GetLast()
+	if err != nil {
+		return nil, fmt.Errorf("LastBlock: %w", err)
+	}
+	return res, nil
+}
+
+func (c *Chain) GetBlockAt(h int) (*BlockResp, error) {
+	res, err := c.NodeAPI.GetBlockAt(h)
+	if err != nil {
+		return nil, fmt.Errorf("GetBlockAt: %w", err)
+	}
+	return res, nil
+}
+
+func (c *Chain) GetBlocksWithin(start, end int) ([]*BlockResp, error) {
+	res, err := c.NodeAPI.GetBlocksWithin(start, end)
+	if err != nil {
+		return nil, fmt.Errorf("GetBlocksWithin: %w", err)
+	}
+	return res, nil
+}
