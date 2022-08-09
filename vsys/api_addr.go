bdea4c8f6322d36e9e7a2129c56e13bcf0401644
poiug07vsys <106374617+poiug07vsys@users.noreply.github.com>
2022-08-09 13:57:17 +0800

    feat: api_addr

--- vsys/api_addr.go
+++ vsys/api_addr.go
@@ -13,6 +13,7 @@ type BalDetails struct {
 	Height     Height `json:"height"`
 }
 
+// GetBalDetails gets the balance details of the given address.
 func (na *NodeAPI) GetBalDetails(addr string) (*BalDetails, error) {
 	res := &BalDetails{}
 	resp, err := na.R().SetResult(res).Get("/addresses/balance/details/" + addr)
@@ -23,4 +24,52 @@ func (na *NodeAPI) GetBalDetails(addr string) (*BalDetails, error) {
 		return nil, fmt.Errorf("GetBalDetails: %s", resp.String())
 	}
 	return res, nil
+}
+
+func (b *BalDetails) String() string {
+	return fmt.Sprintf("%T(%+v)", b, *b)
+}
+
+type AddrResp struct {
+	Address Str `json:"address"`
+}
+
+// GetAddr gets the address from the public key.
+func (na *NodeAPI) GetAddr(pubKey string) (*AddrResp, error) {
+	res := &AddrResp{}
+	resp, err := na.R().SetResult(res).Get(fmt.Sprintf("/addresses/publicKey/%s", pubKey))
+	if err != nil {
+		return nil, fmt.Errorf("GetAddr: %w", err)
+	}
+	if !resp.IsSuccess() {
+		return nil, fmt.Errorf("GetAddr: %s", resp.String())
+	}
+	return res, nil
+}
+
+func (a *AddrResp) String() string {
+	return fmt.Sprintf("%T(%+v)", a, *a)
+}
+
+type BalResp struct {
+	Address       Str  `json:"address"`
+	Confirmations int  `json:"confirmations"`
+	Balance       VSYS `json:"balance"`
+}
+
+// GetBal gets the ledger(regular) balance of the given address.
+func (na *NodeAPI) GetBal(addr string) (*BalResp, error) {
+	res := &BalResp{}
+	resp, err := na.R().SetResult(res).Get(fmt.Sprintf("/addresses/balance/%s", addr))
+	if err != nil {
+		return nil, fmt.Errorf("GetBal: %w", err)
+	}
+	if !resp.IsSuccess() {
+		return nil, fmt.Errorf("GetBal: %s", resp.String())
+	}
+	return res, nil
+}
+
+func (b *BalResp) String() string {
+	return fmt.Sprintf("%T(%+v)", b, *b)
 }
